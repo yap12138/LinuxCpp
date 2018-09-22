857c1b7232c1f664904199d2f5592bd111196c06
yap12138 <438773897@qq.com>
2018-09-22 10:21:29 +0800

    epoll et模式注释更改

--- epoll_example/epoll_example.hpp
+++ epoll_example/epoll_example.hpp
@@ -12,11 +12,11 @@ namespace hogason
 		using ep_t = int;
 
 		/*
-		 * 1.当用户输入一组字符，这组字符被送入buffer，字符停留在buffer中，又因为buffer由空变为不空，所以ET返回读就绪，输出”welcome to epoll's world！”。
+		 * 1.当用户输入一组字符，这组字符被送入buffer，字符停留在buffer中，又因为buffer由空变为不空，所以ET返回读就绪，输出”Trigger event.”。
 		 * 2.之后程序再次执行epoll_wait，此时虽然buffer中有内容可读，但是根据我们上节的分析，ET并不返回就绪，导致epoll_wait阻塞。
 		 *	（底层原因是ET下就绪fd的epitem只被放入rdlist一次）。
 		 * 3.用户再次输入一组字符，导致buffer中的内容增多，根据我们上节的分析这将导致fd状态的改变，是对应的epitem再次加入rdlist，
-		 *	从而使epoll_wait返回读就绪，再次输出“Welcome to epoll's world！”。
+		 *	从而使epoll_wait返回读就绪，再次输出“Trigger event.”。
 		 */
 		void et_stdin()
 		{
